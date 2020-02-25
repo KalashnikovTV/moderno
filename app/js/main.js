eea04668cac20fdda62a2d40dd787c953c1e757c
KalashnikovTV <max.malaschkov@mail.ru>
2020-02-25 21:04:23 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,33 @@ $(function(){
         dots: true,
         arrows: false,
         slidesToShow: 4,
-        slidesToScroll: 4
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1900,
+                settings: {
+                    dots: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+              }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                    dots: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 801,
+                settings: {
+                    dots: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     });
 
     $(".js-range-slider").ionRangeSlider({
